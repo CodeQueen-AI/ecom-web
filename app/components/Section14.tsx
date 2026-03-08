f0eda969a694f460ff79247f347e336ebc38b62b
CodeQueen-AI <codeq209@gmail.com>
2026-03-08 16:53:24 +0500

    update

--- app/components/Section14.tsx
+++ app/components/Section14.tsx
@@ -2,30 +2,30 @@
 
 export default function Collection() {
   return (
-    <section className="poppins py-20">
-      <h1 className="text-center text-4xl font-semibold mb-12">
+    <section className="py-20">
+      <h1 className="text-center text-7xl font-semibold mb-12 allura">
         Collection
       </h1>
       <div className="w-[70%] mx-auto grid grid-cols-3 gap-5">
-        <div className="relative h-[450px] bg-[url('/rado.jpg')] bg-cover bg-center group">
+        <div className="relative h-[450px] bg-[url('/watch/w20.jpg')] bg-cover bg-center group">
           <Overlay name="Rado" />
         </div>
         <div className="flex flex-col gap-5">
-          <div className="relative h-[215px] bg-[url('/Tissot.jpg')] bg-cover bg-center group">
+          <div className="relative h-[215px] bg-[url('/watch/w21.jpg')] bg-cover bg-center group">
             <Overlay name="Tissot" />
           </div>
-          <div className="relative h-[215px] bg-[url('/cartier.jpg')] bg-cover bg-center group">
+          <div className="relative h-[215px] bg-[url('/watch/w22.jpg')] bg-cover bg-center group">
             <Overlay name="Cartier" />
           </div>
         </div>
         <div className="flex flex-col gap-5">
-          <div className="relative h-[140px] bg-[url('/geneve.jpg')] bg-cover bg-center group">
+          <div className="relative h-[140px] bg-[url('/watch/w23.jpg')] bg-cover bg-center group">
             <Overlay name="Geneve" />
           </div>
-          <div className="relative h-[140px] bg-[url('/omega.jpg')] bg-cover bg-center group">
+          <div className="relative h-[140px] bg-[url('/watch/w24.jpg')] bg-cover bg-center group">
             <Overlay name="Omega" />
           </div>
-          <div className="relative h-[140px] bg-[url('/Fitron.jpg')] bg-cover bg-center group">
+          <div className="relative h-[140px] bg-[url('/watch/w25.jpg')] bg-cover bg-center group">
             <Overlay name="Fitron" />
           </div>
         </div>
