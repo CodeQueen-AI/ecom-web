3b7997a54c2af4d5fc5f63ee91b06224b185dd2f
CodeQueen-AI <codeq209@gmail.com>
2026-03-08 16:21:00 +0500

    update

--- app/components/Section4.tsx
+++ app/components/Section4.tsx
@@ -4,54 +4,29 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <section className="w-full min-h-screen flex overflow-hidden">
-
-      {/* Google Fonts */}
-      <style>
-        {`
-          @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Allura&display=swap');
-
-          .poppins {
-            font-family: 'Poppins', sans-serif;
-          }
-
-          .allura {
-            font-family: 'Allura', cursive;
-          }
-        `}
-      </style>
-
-      {/* LEFT IMAGE */}
       <div className="w-1/2 relative">
         <Image
-          src="/Img9.avif"
+          src="/Img5.avif"
           alt="watch"
           fill
-          className="object-cover"
-        />
+          className="object-cover"/>
       </div>
-
-      {/* RIGHT SIDE */}
       <div className="w-1/2 flex items-center">
         <div className="max-w-md mx-auto poppins">
-
           <h1 className="text-5xl font-semibold text-gray-900 leading-tight">
             THE <span className="allura text-6xl font-normal">Watch</span> THAT
             <br />
             DEFIED TIME
           </h1>
-
           <p className="mt-6 text-gray-600 text-lg">
             Explore the legend of the Hamilton Murph, an iconic watch whose story
             blends cinematic emotion with timeless craftsmanship.
           </p>
-
           <button className="mt-8 px-8 py-3 border border-gray-400 text-gray-800 hover:bg-black hover:text-white font-semibold transition cursor-pointer">
             DISCOVER
           </button>
-
         </div>
       </div>
-
     </section>
   );
 }
