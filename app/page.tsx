d899003c287ccf8c05d23de2aaae276dfde94687
CodeQueen-AI <codeq209@gmail.com>
2026-03-10 00:35:16 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,13 @@ import Sec7 from './components/Sec7'
 import Sec8 from './components/Sec8'
 import Sec9 from './components/Sec9'
 import Sec10 from './components/Sec10'
+import Sec11 from './components/Sec11'
+import Sec12 from './components/Sec12'
 import Section1 from './components/Section1'
 import Section2 from './components/Section2'
 import Section9 from './components/Section9'
 import Section11 from './components/Section11'
 import Section13 from './components/Section13'
-import Section14 from './components/Section14'
 import Footer from './components/footer'
 export default function Page() {
   return (
@@ -31,13 +32,14 @@ export default function Page() {
     <Sec7/>
     <Sec8/>
     <Sec9/>
-    {/* <Sec10/> */}
+    <Sec10/>
+    <Sec11/>
+    <Sec11/>
     <Section1/>
     <Section2/>
     <Section9/>
     <Section11/>
     <Section13/>
-    <Section14/>
     <Footer/>
     </div>
   );
