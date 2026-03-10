58b20d345bcd04db6eec87ab89c54c047dbc968d
CodeQueen-AI <codeq209@gmail.com>
2026-03-10 17:49:06 +0500

    update

--- frontend/app/products/page.tsx
+++ frontend/app/products/page.tsx
@@ -9,19 +9,19 @@ const products = [
   { id: 3, name: "Sporty Chrono", price: "$300", img: "/products/p1.jpg" },
   { id: 4, name: "Elegant Black", price: "$350", img: "/products/p1.jpg" },
 ];
-
 export default function ProductsPage() {
   return (
     <section className="w-full min-h-screen px-16 py-12 poppins">
-
-      <h1 className="text-4xl font-semibold mb-10 text-center">
+      <h1 className="text-7xl font-extralight mb-10 text-center allura">
         Our Products
       </h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
 
         {products.map((product) => (
-          <div key={product.id} className="flex flex-col items-center bg-white p-4 shadow-md hover:shadow-lg transition">
+          <div
+            key={product.id}
+            className="flex flex-col items-start border border-gray-300 p-4 transition">
             
             {/* Image */}
             <div className="w-full h-60 relative mb-4">
@@ -29,22 +29,21 @@ export default function ProductsPage() {
                 src={product.img}
                 alt={product.name}
                 fill
-                className="object-contain"
-              />
+                className="object-contain"/>
             </div>
 
             {/* Name */}
-            <h2 className="text-lg font-medium mb-2">{product.name}</h2>
+            <h2 className="text-xl font-medium mb-2">{product.name}</h2>
 
-            {/* Price + View */}
+            {/* Button + Price */}
             <div className="flex items-center justify-between w-full mt-auto">
-              <span className="font-semibold">{product.price}</span>
               <Link
                 href={`/products/${product.id}`}
                 className="px-4 py-1 border border-black bg-black text-white hover:bg-white hover:text-black transition cursor-pointer text-sm"
               >
                 View
               </Link>
+              <span className="font-semibold text-3xl">{product.price}</span>
             </div>
 
           </div>
