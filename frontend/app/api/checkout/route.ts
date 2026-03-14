8ce49be6e30374d5900bc62815c82ebd1486aba9
CodeQueen-AI <codeq209@gmail.com>
2026-03-14 13:29:53 +0500

    update

--- frontend/app/api/checkout/route.ts
+++ frontend/app/api/checkout/route.ts
@@ -1,58 +1,3 @@
-// import Stripe from "stripe";
-// import { NextResponse } from "next/server";
-
-// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-//   apiVersion: "2025-07-30.basil",
-// });
-
-// export async function POST(req: Request) {
-//   try {
-//     const { cartItems } = await req.json();
-
-//     const line_items = cartItems.map((item: any) => ({
-//       price_data: {
-//         currency: "usd",
-//         product_data: {
-//           name: item.name,
-//           images: [
-//             item.image.startsWith("http")
-//               ? item.image
-//               : `http://localhost:3000/${item.image}`,
-//           ],
-//         },
-//         unit_amount: item.price * 100, 
-//       },
-//       quantity: item.quantity,
-//     }));
-
-//     const session = await stripe.checkout.sessions.create({
-//       payment_method_types: ["card"],
-//       line_items,
-//       mode: "payment",
-
-//       // Payment successful hone par
-//       success_url: `${req.headers.get("origin")}/success`,
-
-//       // Payment reject / cancel hone par
-//       cancel_url: `${req.headers.get("origin")}/reject`,
-//     });
-
-//     return NextResponse.json({ url: session.url });
-
-//   } catch (err: any) {
-//     console.error("Stripe Error:", err);
-//     return NextResponse.json(
-//       { error: err.message },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-
-
-
-
-
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
@@ -85,10 +30,8 @@ export async function POST(req: Request) {
       line_items,
       mode: "payment",
 
-      // success
       success_url: `${req.headers.get("origin")}/cart?success=true`,
 
-      // reject / cancel
       cancel_url: `${req.headers.get("origin")}/cart?reject=true`,
     });
 
